7f8af81202591314c38a5a7b2bbc4d94da93f636
Ricky Blake <rab0196@yahoo.com>
2020-10-02 09:14:36 -0500

    quick fix

--- scripts/retailers/Retailer.js
+++ scripts/retailers/Retailer.js
@@ -1,6 +1,6 @@
 export const RetailerHTML = (retailer) => {
     return `
-        <div class="flower">
+        <div class="retailer">
             <h3>${retailer.name}</h3>
             <p>Location: ${retailer.address}<br/>${retailer.city}, ${retailer.state}</p>
         <div>
